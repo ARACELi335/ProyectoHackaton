cc66eec0979d1e1f8a998a722b41a5e74fb9d137
ARACELi335 <112772376+ARACELi335@users.noreply.github.com>
2024-04-16 19:19:27 -0300

    Add files via upload

---
+++ Dominio/Sistema.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Sistema
+    {
+        private List<Usuario> usuarios { get; set; } = new List<Usuario>();
+        private List<Proyecto> proyectos { get; set; } = new List<Proyecto>();
+
+        //Patrón Singleton
+        private static Sistema instance = null;
+        private Sistema() { Precarga();}
+        public static Sistema GetSistema()
+        {
+            if (instance == null)
+            {
+                instance = new Sistema();
+            }
+            return instance;
+        }
+
+        //Precarga de datos, No es una aplicación oficial, por lo tanto no contiene datos reales.
+        private void Precarga()
+        {
+            CreateUsuario(new Usuario("Erika", "Araceli1", "Junior"));
+            GetUsuarioById(0).Aptitudes.Add(new Aptitud("java"));
+            GetUsuarioById(0).Aptitudes.Add(new Aptitud("javascript"));
+            GetUsuarioById(0).Aptitudes.Add(new Aptitud("c#"));
+            GetUsuarioById(0).Aptitudes.Add(new Aptitud("css"));
+            GetUsuarioById(0).Aptitudes.Add(new Aptitud("html"));
+        }
+
+        public void CreateUsuario(Usuario u)
+        {
+            if (usuarios != null)
+            {
+                foreach (Usuario usuario in usuarios)
+                {
+                    if (u.Nombre.Equals(usuario.Nombre))
+                    {
+                        throw new Exception("Este usuario ya se encuentra registrado.");
+                    }
+                }
+            }
+            try
+            {
+                u.Validar();
+                usuarios.Add(u);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Usuario Login(string nombre, string password)
+        {
+            if (usuarios == null) { return null; }
+            foreach (Usuario usuario in usuarios)
+            {
+                if (usuario.Nombre.Equals(nombre) && usuario.Password.Equals(password))
+                {
+                    return usuario;
+                }
+            }
+            return null;
+        }
+
+        public Usuario GetUsuarioById(int? id)
+        {
+            if(usuarios != null)
+            {
+                foreach(Usuario usuario in usuarios)
+                {
+                    if(usuario.Id == id)
+                    {
+                        return usuario;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public static int LastId { get; set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public List<Aptitud> Aptitudes { get; set; } = new List<Aptitud>();
+        public List<Proyecto> MisProyectos { get; set; } = new List<Proyecto>();
+        public List<Proyecto> Proyectos { get; set; } = new List<Proyecto>(); //Proyectos de otras personas en los que el ususario ayudó
+        public List<Notificacion> Notificaciones { get; set; } = new List<Notificacion>();
+        public string Nivel { get; set; } //Junior, semi senior, senior
+
+        public Usuario()
+        {
+            Id = LastId;
+            LastId++;
+        }
+
+        public Usuario(string nombre, string password, string nivel)
+        {
+            Id = LastId;
+            LastId++;
+            Nombre = nombre;
+            Password = password;
+            Nivel = nivel;
+        }
+
+        public void Validar()
+        {
+            if (Nombre == null | Password == null) { throw new Exception("Todos los campos deben ser completados."); }
+            else if (Nivel == "X") { throw new Exception("Debe seleccionar un nivel."); }
+            else if (Password.Length < 8){ throw new Exception("La contraseña debe tener al menos 8 caracteres."); }
+            else if (!Password.Any(char.IsUpper) | !Password.Any(char.IsLower) | !Password.Any(char.IsDigit))
+            {
+                throw new Exception("La contraseña debe tener al menos una letra mayúscula, una minúscula y un número");
+            }
+        }
+    }
+}
