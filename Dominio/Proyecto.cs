395ee4c4a5a690aab42d8b1c345feab90efdbd55
ARACELi335 <112772376+ARACELi335@users.noreply.github.com>
2024-04-17 17:32:53 -0300

    Update Proyecto.cs

--- Dominio/Proyecto.cs
+++ Dominio/Proyecto.cs
@@ -1,6 +1,40 @@
-﻿namespace Dominio
+namespace Dominio
 {
     public class Proyecto
     {
+        public static int LastId { get; set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Autor {  get; set; }
+        public string Estado { get; set; } //En progreso, En pausa, Finalizado
+        public string Descripcion { get; set; }
+        public List<Aptitud> Tecnologias { get; set; } = new List<Aptitud>(); //Tecnologías utilizadas para el proyecto
+        public List<Archivos> Archivos { get; set; } = new List<Archivos>();
+        public List<Consejo> Consejos { get; set; } = new List<Consejo>();
+        public List<Usuario> Ayudantes { get; set; } = new List<Usuario>(); //Usuarios que aportaron archivos al proyecto
+        public List<Usuario> Consejeros { get; set; } = new List<Usuario>(); //Usuarios que aportaron consejos al proyecto
+        public string Tipo { get; set; } //Público o privado
+
+        public Proyecto() { 
+            Id = LastId;
+            LastId++;
+        }
+
+        public Proyecto(string nombre, string descripcion, string tipo)
+        {
+            Id = LastId;
+            LastId++;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Tipo = tipo;
+        }
+
+        public void Validar()
+        {
+            if (Nombre == null | Descripcion == null | Tecnologias.Count == 0)
+            {
+                throw new Exception("Todos los campos deben ser completados.");
+            }
+        }
     }
 }
