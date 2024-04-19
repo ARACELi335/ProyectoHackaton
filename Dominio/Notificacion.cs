b2ab686bba30d41184c0381aee7d036637527c71
ARACELi335 <112772376+ARACELi335@users.noreply.github.com>
2024-04-19 16:19:01 -0300

    Update Notificacion.cs

--- Dominio/Notificacion.cs
+++ Dominio/Notificacion.cs
@@ -1,6 +1,29 @@
-﻿namespace Dominio
+namespace Dominio
 {
     public class Notificacion
     {
+        public static int LastId { get; set; }
+        public int Id { get; set; }
+        public int User {  get; set; } //Usuario al que pertenece la notificación
+        public string Autor {  get; set; } //Persona de la que viene la notificación
+        public Proyecto Proyecto { get; set; }
+        public string Tipo { get; set; } //Ayuda, Consejo o Archivo subido a tu proyecto
+        public string Estado { get; set; } //Por leer, Leídas
+
+        public Notificacion()
+        {
+            Id = LastId;
+            LastId++;
+        }
+        public Notificacion(int user, string autor, Proyecto proyecto, string tipo, string estado)
+        { 
+            Id = LastId;
+            LastId++;
+            User = user;
+            Autor = autor;
+            Proyecto = proyecto;
+            Tipo = tipo;
+            Estado = estado;
+        }
     }
 }
