60ceab50779261d5cfcaa7430b77348205fc4973
ARACELi335 <112772376+ARACELi335@users.noreply.github.com>
2024-04-22 23:13:37 -0300

    Update Consejo.cs

--- Dominio/Consejo.cs
+++ Dominio/Consejo.cs
@@ -1,6 +1,15 @@
-﻿namespace Dominio
+namespace Dominio
 {
     public class Consejo
     {
+        public Usuario Autor {  get; set; }
+        public string Mensaje { get; set; }
+
+        public Consejo() { }
+        public Consejo(Usuario autor, string mensaje)
+        {
+            Autor = autor;
+            Mensaje = mensaje;
+        }
     }
 }
